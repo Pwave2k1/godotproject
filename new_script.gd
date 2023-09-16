7f86fe8d100e4b6d8c081eceaa5436be272ddcf3
Hồ Hữu Phước <hophuoc56@gmail.com>
2023-09-16 11:13:04 +0700

    wip

--- new_script.gd
+++ new_script.gd
@@ -1,11 +1,14 @@
-extends CharacterBody2D
+extends CharacterBody2D 
+ 
 
+var move_speed = 100 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func    _ready():
+    pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rotate(0.5 *delta)
+func           _process(delta):
+	
+                     rotate(0.5 *delta)
+    
+func _getInPut() -> void:
+    pass
