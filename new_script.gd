241660432fb683911d15676bc036341d7dfd5901
Hồ Hữu Phước <hophuoc56@gmail.com>
2023-09-16 12:20:52 +0700

    ttt

--- new_script.gd
+++ new_script.gd
@@ -3,15 +3,9 @@ extends CharacterBody2D
 var move_speed = 100
 
 
-func     _ready():
-	
+func _ready():
 	pass
 
 
-func 		process(delta):
-	rotate(0.5 * delta)
-
-
-func _getInPut() -> void:
-	
+func        _process(delta):
 	pass
